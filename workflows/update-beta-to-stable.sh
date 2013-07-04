4a1716d73eabdeca96bcdd9c82c3274eb3b82f72
Cherian Mathew <cherianm@gmail.com>
2013-07-04 16:31:48 +0200

    update version and documentation for beta, stable workflow and update script

--- workflows/update-beta-to-stable.sh
+++ workflows/update-beta-to-stable.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-
 cp ./data_refinement_workflow.beta.t2flow ./data_refinement_workflow.t2flow
 sed -i 's/http:\/\/ww2\.bgbm\.org\/biovel\/public\/beta\(.*\)/http:\/\/ww2\.bgbm\.org\/biovel\/public\1/g' data_refinement_workflow.t2flow
 sed -i 's/-SNAPSHOT//g' data_refinement_workflow.t2flow
+sed -i 's/\[BETA\]\ Data\ Refinement\ Workflow/Data\ Refinement\ Workflow/g' data_refinement_workflow.t2flow
+sed -i 's/\[BETA\]\ Data\ Refinement\ Workflow/Data\ Refinement\ Workflow/g' data_refinement_workflow.t2flow
+sed -i 's/This\ is\ a\ beta\ version\ of\ the\ Data\ Refinement\ Workflow\ and\ should\ be\ used\ only\ for\ review\ and\ testing\.//g' data_refinement_workflow.t2flow
+
 
 
