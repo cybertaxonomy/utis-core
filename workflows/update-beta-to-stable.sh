74c0253ce74f41e52deaa9bbf47b1338d4c6e39d
Cherian Mathew <cherianm@gmail.com>
2013-07-04 16:15:37 +0200

    update script now includes change of drf-workflow artifact version updated data_refinement_workflow.t2flow to make it stable

--- workflows/update-beta-to-stable.sh
+++ workflows/update-beta-to-stable.sh
@@ -3,5 +3,6 @@
 
 cp ./data_refinement_workflow.beta.t2flow ./data_refinement_workflow.t2flow
 sed -i 's/http:\/\/ww2\.bgbm\.org\/biovel\/public\/beta\(.*\)/http:\/\/ww2\.bgbm\.org\/biovel\/public\1/g' data_refinement_workflow.t2flow
+sed -i 's/-SNAPSHOT//g' data_refinement_workflow.t2flow
 
 
