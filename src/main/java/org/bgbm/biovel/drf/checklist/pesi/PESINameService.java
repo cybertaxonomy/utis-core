5529882a100403e693a6cbbaa642ebfea66baf2d
Cherian Mathew <cherianm@gmail.com>
2013-09-19 16:36:41 +0200

    initial commit for PESI related stuff

---
+++ src/main/java/org/bgbm/biovel/drf/checklist/pesi/PESINameService.java
@@ -0,0 +1,16 @@
+/**
+ * PESINameService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package org.bgbm.biovel.drf.checklist.pesi;
+
+public interface PESINameService extends javax.xml.rpc.Service {
+    public java.lang.String getPESINameServicePortAddress();
+
+    public PESINameServicePortType getPESINameServicePort() throws javax.xml.rpc.ServiceException;
+
+    public PESINameServicePortType getPESINameServicePort(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
