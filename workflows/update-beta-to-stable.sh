96ca782d9e1ce32a18c4f2cbbe3c19f1211caf4b
Cherian Mathew <cherianm@gmail.com>
2014-02-04 16:08:31 +0100

    data_refinement_workflow.beta.t2flow : corrected workflow name update-beta-to-stable.sh : added versioning for insteraction pages

--- workflows/update-beta-to-stable.sh
+++ workflows/update-beta-to-stable.sh
@@ -10,7 +10,7 @@ version=$1
 cp ./data_refinement_workflow.beta.t2flow ./data_refinement_workflow.t2flow
 sed -i "s/http:\/\/ww2\.bgbm\.org\/biovel\/public\/beta\/webui\(.*\)/http:\/\/ww2\.bgbm\.org\/biovel\/public\/webui\/$version\1/g" data_refinement_workflow.t2flow
 sed -i 's/-SNAPSHOT//g' data_refinement_workflow.t2flow
-sed -i 's/\[BETA\]\ Data\ Refinement\ Workflow/Data\ Refinement\ Workflow/g' data_refinement_workflow.t2flow
+sed -i 's/__BETA__Data_Refinement/Data_Refinement/g' data_refinement_workflow.t2flow
 sed -i 's/\[BETA\]\ Data\ Refinement\ Workflow/Data\ Refinement\ Workflow/g' data_refinement_workflow.t2flow
 sed -i 's/This\ is\ a\ beta\ version\ of\ the\ Data\ Refinement\ Workflow\ and\ should\ be\ used\ only\ for\ review\ and\ testing\.//g' data_refinement_workflow.t2flow
 
