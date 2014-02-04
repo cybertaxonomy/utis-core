dc76e4f051d651456de6afa948b73a6d866064ab
Cherian Mathew <cherianm@gmail.com>
2014-02-04 10:27:14 +0100

    data_refinement_workflow.beta.t2flow :  - added refine server check  - added possibility to use external refine server  - moved refine server calls to jar
    update-beta-to-stable.sh
     - added versioning possibility

--- workflows/update-beta-to-stable.sh
+++ workflows/update-beta-to-stable.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+  then
+    echo "Release version not provided"
+fi
+
+version=$1
+
 cp ./data_refinement_workflow.beta.t2flow ./data_refinement_workflow.t2flow
-sed -i 's/http:\/\/ww2\.bgbm\.org\/biovel\/public\/beta\(.*\)/http:\/\/ww2\.bgbm\.org\/biovel\/public\1/g' data_refinement_workflow.t2flow
+sed -i "s/http:\/\/ww2\.bgbm\.org\/biovel\/public\/beta\/webui\(.*\)/http:\/\/ww2\.bgbm\.org\/biovel\/public\/webui\/$version\1/g" data_refinement_workflow.t2flow
 sed -i 's/-SNAPSHOT//g' data_refinement_workflow.t2flow
 sed -i 's/\[BETA\]\ Data\ Refinement\ Workflow/Data\ Refinement\ Workflow/g' data_refinement_workflow.t2flow
 sed -i 's/\[BETA\]\ Data\ Refinement\ Workflow/Data\ Refinement\ Workflow/g' data_refinement_workflow.t2flow
