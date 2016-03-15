027c8dbcb2a5474931c2a70ad56aad5ee85f6c6f
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2016-03-15 15:09:10 +0100

    version specific store locations

--- src/main/java/org/cybertaxonomy/utis/store/Store.java
+++ src/main/java/org/cybertaxonomy/utis/store/Store.java
@@ -9,11 +9,15 @@
 */
 package org.cybertaxonomy.utis.store;
 
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipUtils;
@@ -38,23 +42,39 @@ public abstract class Store {
     protected static final Logger logger = LoggerFactory.getLogger(Store.class);
     private static final File tmpDir = new File(System.getProperty("java.io.tmpdir"));
     private static final File userHomeDir = new File(System.getProperty("user.home"));
+    private static final String PROJECT_VERSION_KEY = "project.version";
+    protected static String VERSION = "0.0-DEFAULT";
     protected static File utisHome = null;
+    private static final Pattern majorMinorPattern = Pattern.compile("^(\\d+\\.\\d+).*$");
 
     static {
+            Properties versionProps = new Properties();
+            InputStream propsResource = Store.class.getResourceAsStream("/version.properties");
+            if(propsResource == null) {
+                logger.error("No resource named 'version.properties' found, using default version '0.0-DEFAULT'");
+            }
+            try {
+            versionProps.load(propsResource);
+            VERSION = versionProps.get(PROJECT_VERSION_KEY).toString();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         if(System.getProperty("utis.home") != null){
             utisHome = new File(System.getProperty("utis.home"));
             logger.info("utis.home defined by system property: " + utisHome.getAbsolutePath());
         } else {
             // this is the folder for production mode
             // /var/lib/utis must be created prior starting the application
-            utisHome = new File("/var/lib/utis");
+            utisHome = new File("/var/lib/utis" + File.separator + majorMinorVersion());
             logger.info("utis.home " + utisHome.getAbsolutePath());
             if(!utisHome.canWrite()) {
                 logger.info("utis.home " + utisHome.getAbsolutePath() +  " is not writable, trying other location ...");
                 // this is the folder for development mode
                 // if the application is started inside a service as jetty
                 // this folder will most probably not be writable.
-                utisHome = new File(userHomeDir, ".utis");
+                utisHome = new File(userHomeDir, ".utis" + File.separator + majorMinorVersion());
             }
             logger.info("utis.home finally is " + utisHome.getAbsolutePath());
         }
@@ -71,9 +91,18 @@ public abstract class Store {
     }
 
     /**
-     * @return
+     * @return the Major.Minor version number of the project version,
+     * or the full VERSION string if it is not containing a Major.Minor version number
      */
     protected abstract String storeName();
+
+    protected static String majorMinorVersion() {
+        Matcher matcher = majorMinorPattern.matcher(VERSION);
+        if(matcher.matches() && matcher.groupCount() == 1) {
+            return matcher.group(1);
+        }
+        return VERSION;
+    }
 
     /**
      * @param rdfFileUri
