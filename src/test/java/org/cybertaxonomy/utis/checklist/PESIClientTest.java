3fe30b48ce76f89317a78b2f3309431c4c9a8f7f
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2016-10-19 12:01:45 +0200

    test for PESIClient

--- src/test/java/org/cybertaxonomy/utis/checklist/PESIClientTest.java
+++ src/test/java/org/cybertaxonomy/utis/checklist/PESIClientTest.java
@@ -1,18 +1,19 @@
 package org.cybertaxonomy.utis.checklist;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.xml.bind.JAXBException;
 
-import org.cybertaxonomy.utis.checklist.DRFChecklistException;
-import org.cybertaxonomy.utis.checklist.PESIClient;
-import org.cybertaxonomy.utis.checklist.SearchMode;
-import org.cybertaxonomy.utis.checklist.Species2000ColClient;
 import org.cybertaxonomy.utis.client.ServiceProviderInfo;
 import org.cybertaxonomy.utis.input.DRFCSVInputParser;
 import org.cybertaxonomy.utis.input.DRFInputException;
+import org.cybertaxonomy.utis.tnr.msg.Response;
+import org.cybertaxonomy.utis.tnr.msg.Synonym;
 import org.cybertaxonomy.utis.tnr.msg.TnrMsg;
 import org.cybertaxonomy.utis.utils.BiovelUtils;
 import org.cybertaxonomy.utis.utils.JSONUtils;
@@ -20,13 +21,20 @@ import org.cybertaxonomy.utis.utils.TnrMsgException;
 import org.cybertaxonomy.utis.utils.TnrMsgUtils;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PESIClientTest {
+
+    protected static final Logger logger = LoggerFactory.getLogger(PESIClientTest.class);
 
     private static DRFCSVInputParser parser;
     private static List<String> nameCompleteList;
 
     private static ServiceProviderInfo ci;
+
+
+    static PESIClient client;
 
     @BeforeClass
     public static void  setup() {
@@ -45,6 +53,10 @@ public class PESIClientTest {
                 Species2000ColClient.LABEL,
                 Species2000ColClient.URL,
                 Species2000ColClient.DATA_AGR_URL, ServiceProviderInfo.DEFAULT_SEARCH_MODE);
+
+        client =  new PESIClient();
+        client.setChecklistInfo(client.buildServiceProviderInfo());
+
     }
 
     @Test
@@ -70,6 +82,42 @@ public class PESIClientTest {
             String outputXML = TnrMsgUtils.convertTnrMsgToXML(tnrMsg);
             System.out.println(outputXML);
         }
+    }
+
+    @Test
+    public void exactScientificNameTest() throws DRFChecklistException, TnrMsgException {
+
+        TnrMsg tnrMsg = TnrMsgUtils.createRequest(SearchMode.scientificNameExact, "Lactuca virosa", true);
+        client.queryChecklist(tnrMsg);
+        String outputXML = TnrMsgUtils.convertTnrMsgToXML(tnrMsg);
+
+        System.out.println(outputXML);
+        assertEquals(2, tnrMsg.getQuery().get(0).getResponse().size());
+
+        Response response1 = tnrMsg.getQuery().get(0).getResponse().get(0);
+        assertEquals("Lactuca virosa", response1.getMatchingNameString());
+        assertEquals("Lactuca virosa", response1.getTaxon().getTaxonName().getCanonicalName());
+        logger.info("Accepted: " + response1.getTaxon().getTaxonName().getScientificName() + " (" + response1.getTaxon().getUrl() + ")");
+        assertTrue(response1.getTaxon().getUrl() != null);
+        assertTrue(response1.getTaxon().getIdentifier() != null);
+        assertEquals("L.", response1.getTaxon().getTaxonName().getAuthorship());
+        assertTrue(response1.getSynonym().size() > 0);
+        for(Synonym syn : response1.getSynonym()) {
+            logger.info("Synonym: " + syn.getTaxonName().getScientificName() + " (" + syn.getUrl() + ")");
+        }
+
+        Response response2 = tnrMsg.getQuery().get(0).getResponse().get(1);
+        assertEquals("Lactuca virosa", response2.getMatchingNameString());
+        assertEquals("Lactuca serriola", response2.getTaxon().getTaxonName().getCanonicalName());
+        logger.info("Accepted: " + response2.getTaxon().getTaxonName().getScientificName() + " (" + response2.getTaxon().getUrl() + ")");
+        assertTrue(response2.getTaxon().getUrl() != null);
+        assertTrue(response2.getTaxon().getIdentifier() != null);
+        assertEquals("L.", response2.getTaxon().getTaxonName().getAuthorship());
+        assertTrue(response2.getSynonym().size() > 0);
+        for(Synonym syn : response2.getSynonym()) {
+            logger.info("Synonym: " + syn.getTaxonName().getScientificName() + " (" + syn.getUrl() + ")");
+        }
+
     }
 }
 
