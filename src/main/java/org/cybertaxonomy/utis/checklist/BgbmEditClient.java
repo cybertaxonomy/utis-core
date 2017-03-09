65ef0bd4c4cc4053573bd9081957fd2eea934702
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2017-03-09 12:32:37 +0100

    enabling edit-test as server

--- src/main/java/org/cybertaxonomy/utis/checklist/BgbmEditClient.java
+++ src/main/java/org/cybertaxonomy/utis/checklist/BgbmEditClient.java
@@ -48,12 +48,12 @@ public class BgbmEditClient extends AggregateChecklistClient<RestClient> {
     //private static final HttpHost HTTP_HOST = new HttpHost("api.cybertaxonomy.org", 80);
 
     // edit-test
-    //private static final String SERVER_PATH_PREFIX = "/cdmserver/";
-    //private static final HttpHost HTTP_HOST = new HttpHost("test.e-taxonomy.eu", 80);
+    private static final String SERVER_PATH_PREFIX = "/cdmserver/";
+    private static final HttpHost HTTP_HOST = new HttpHost("test.e-taxonomy.eu", 80);
 
     // localhost
-    private static final String SERVER_PATH_PREFIX = "/";
-    private static final HttpHost HTTP_HOST = new HttpHost("localhost", 8080);
+//    private static final String SERVER_PATH_PREFIX = "/";
+//    private static final HttpHost HTTP_HOST = new HttpHost("localhost", 8080);
 
 
 
