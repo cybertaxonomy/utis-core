15baf16b3efbd1137dcbe0658efd2c768a3e635f
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2019-07-22 18:13:14 +0200

    AbstractCdmServerClient responses with taxon.url, ref #8394 - switching back to production server

--- src/main/java/org/cybertaxonomy/utis/checklist/PhycobankClient.java
+++ src/main/java/org/cybertaxonomy/utis/checklist/PhycobankClient.java
@@ -19,16 +19,16 @@ public class PhycobankClient extends AbstractCdmServerClient {
     public static final String COPYRIGHT_URL = "http://cybertaxonomy.eu/cdmlib/license.html";
 
     // phycobank-production
-    // private static final String SERVER_PATH_PREFIX = "/";
-    // private static final HttpHost HTTP_HOST = new HttpHost("api.phycobank.org", 80);
+    private static final String SERVER_PATH_PREFIX = "/";
+    private static final HttpHost HTTP_HOST = new HttpHost("api.phycobank.org", 80);
 
     // edit-test
     // private static final String SERVER_PATH_PREFIX = "/cdmserver/";
     // private static final HttpHost HTTP_HOST = new HttpHost("test.e-taxonomy.eu", 80);
 
     // localhost
-    private static final String SERVER_PATH_PREFIX = "/";
-    private static final HttpHost HTTP_HOST = new HttpHost("localhost", 8080);
+    //private static final String SERVER_PATH_PREFIX = "/";
+    //private static final HttpHost HTTP_HOST = new HttpHost("localhost", 8080);
 
 
     public static final EnumSet<SearchMode> SEARCH_MODES = EnumSet.of(
